107a1f6363febddb92c2e871dadc893b6ea45d14
Edward Reed <relentlessreed@gmail.com>
2021-06-05 07:42:48 -0500

    Created variables/Set Up Middleware

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const PORT = process.env.PORT || 3000;
+// Creating Our Variables
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 8080;
 
+// Our Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
 
-app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT: " + PORT);
+});
